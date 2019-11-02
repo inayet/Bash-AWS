f23912d56d3e22b700cedd5c9987276a08f7f11c
Inayet Hadi <inayet@htcsmarketing.com>
2019-11-01 19:53:46 -0600

    Update turnoffec2.sh

--- turnoffec2.sh
+++ turnoffec2.sh
@@ -1,23 +1,27 @@
+# When executing this script in linux to run dos2unix filename to correct the
+# Run dos2unix filename command for the script to run fine in linux since this script was edited in windows
+
+
+#Gets every region in AWS. 
 regions=`aws ec2 describe-regions --query Regions[*].RegionName --output text`
- # Get all instances ids that match the filters
+
 
 for region in $regions
 do
+ # Get all instances ids that match the filters
 instanceids=`aws ec2 describe-instances --region $region --filters 'Name=instance-type, Values=t2.micro' --query 'Reservations[*].Instances[*].InstanceId' --output text`
-echo "this is the region: $region"
-    
+
     # For each instance id loop through it
     for instanceid in $instanceids
     do
-    echo "this is the instance id: $instanceid"
-    # This command stops all instances with the specific id
-    ##aws ec2 stop-instances --region $region --instance-ids $instanceid ;
-    
+    # To stops all instances with the specific id
+   # aws ec2 stop-instances --region $region --instance-ids $instanceid ;
+
     # To start all ec2 uncomment the lin below 
       aws ec2 start-instances --region $region --instance-ids $instanceid ;
 
     done
-    
+
 done
 
 
